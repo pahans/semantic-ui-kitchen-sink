946274255259e3cbde4d75b566bbba50059c96f1
Pahan Sarathchandra <pahan@wso2.com>
2018-02-20 10:49:54 +0530

    Add sample images

--- src/kitchen-sink.jsx
+++ src/kitchen-sink.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Container, Divider, Grid, Header, Image, Menu, Segment } from 'semantic-ui-react';
+import { Button, Container, Divider, Grid, Header, Image, Menu, Segment, Modal } from 'semantic-ui-react';
 
 class KitchenSink extends React.Component {
 
@@ -211,6 +211,22 @@ class KitchenSink extends React.Component {
                             <Button color='blue' inverted>Colored</Button>
                             <Button basic color='blue' inverted>Basic Colored</Button>
                         </Segment>
+                    </Grid.Column>
+                </Grid>
+                <Header as='h2' dividing>Modal</Header>
+                <Grid columns='equal'>
+                    <Grid.Column> 
+                    <Modal trigger={<Button>Show Modal</Button>}>
+                        <Modal.Header>Select a Photo</Modal.Header>
+                        <Modal.Content image>
+                        <Image wrapped size='medium' src='/assets/images/avatar/large/rachel.png' />
+                        <Modal.Description>
+                            <Header>Default Profile Image</Header>
+                            <p>We've found the following gravatar image associated with your e-mail address.</p>
+                            <p>Is it okay to use this photo?</p>
+                        </Modal.Description>
+                        </Modal.Content>
+                    </Modal>
                     </Grid.Column>
                 </Grid>
             </Container>
